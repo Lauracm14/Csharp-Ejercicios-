f1f8f31e27fdaef22d2397b6dadff1c65084ab5a
Lauracm14 <laura14168@hotmail.com>
2025-09-24 22:19:06 -0500

    Solucion5

--- PositivePower/Program.cs
+++ PositivePower/Program.cs
@@ -2,11 +2,29 @@
 {
     static void Main()
     {
-        Console.Write("Ingrese el radio del círculo: ");
-        double radio = double.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Ingrese un número entre 1 y 7: ");
+        int dia = int.Parse(Console.ReadLine() ?? "0");
 
-        double perimetro = 2 * Math.PI * radio;
-
-        Console.WriteLine($"Resultado: {perimetro:F2}");
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("Lunes");
+                break;
+            case 2:
+                Console.WriteLine("Martes");
+                break;
+            case 3:
+                Console.WriteLine("Miércoles");
+                break;
+            case 4:
+                Console.WriteLine("Jueves");
+                break;
+            case 5:
+                Console.WriteLine("Viernes");
+                break;
+            default:
+                Console.WriteLine("Número fuera del rango laboral.");
+                break;
+        }
     }
 }
