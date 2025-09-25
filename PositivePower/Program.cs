ca8e8af12d627d28ffd983d51856cb70b3a72808
Lauracm14 <laura14168@hotmail.com>
2025-09-24 22:32:36 -0500

    Solucion8

--- PositivePower/Program.cs
+++ PositivePower/Program.cs
@@ -2,20 +2,16 @@
 {
     static void Main()
     {
-        Console.Write("Ingrese el primer número: ");
-        int num1 = int.Parse(Console.ReadLine() ?? "0");
+        int suma = 0;
 
-        Console.Write("Ingrese el segundo número: ");
-        int num2 = int.Parse(Console.ReadLine() ?? "1"); // evitamos división por 0
+        for (int i = 1; i <= 50; i++)
+        {
+            if (i % 2 == 0)
+            {
+                suma += i;
+            }
+        }
 
-        if (num2 == 0)
-        {
-            Console.WriteLine("No se puede dividir entre 0.");
-        }
-        else
-        {
-            int residuo = num1 % num2;
-            Console.WriteLine($"Resultado: {residuo}");
-        }
+        Console.WriteLine($"Resultado: {suma}");
     }
 }
