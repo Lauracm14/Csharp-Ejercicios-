f1bcc6a67982614e6c1150f2378868a15d315012
Lauracm14 <laura14168@hotmail.com>
2025-09-24 21:29:02 -0500

    Solucion 1

--- PositivePower/Program.cs
+++ PositivePower/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            int resultado = numero * numero; // elevar al cuadrado
+            Console.WriteLine("Resultado: " + resultado);
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("Resultado: Número negativo.");
+        }
+        else // cuando es 0
+        {
+            Console.WriteLine("Resultado: 0");
+        }
+    }
+}
